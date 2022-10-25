87b925ea910db099bd959040dee1d68a86dc09b5
MetaInfix <metainfix@gmail.com>
2022-10-25 18:33:09 +0530

    Updated

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ randomBtn.addEventListener("click", async () => {
   await fetch("https://api.adviceslip.com/advice")
     .then((data) => data.json())
     .then((res) => {
-      console.log(res);
       advice.innerHTML = res.slip.advice;
       adviceId.innerHTML = res.slip.id;
-      if (dice.style.transform === "rotate(0deg)") {
+      if (
+        dice.style.transform === "rotate(0deg)" ||
+        dice.style.transform === ""
+      ) {
         dice.style.transform = "rotate(360deg)";
       } else {
         dice.style.transform = "rotate(0deg)";
